26efeda3071dea6f00cd9a2337c49e64509a05b7
ka-til <akatil@edu.uwaterloo.ca>
2021-12-22 03:52:18 -0500

    New Pipeline Search

---
+++ Akanksha/chaining_mass_ligh_dark_source_pipeline.py
@@ -0,0 +1,374 @@
+"""
+Pipelines: Light Parametric + Mass Light Dark + Source Inversion
+================================================================
+
+By chaining together five searches this script fits `Imaging` dataset of a 'galaxy-scale' strong lens, where in the
+final model:
+
+ - The lens galaxy's light is a parametric bulge+disk `EllSersic` and `EllExponential`.
+ - The lens galaxy's stellar mass distribution is a bulge+disk tied to the light model above.
+ - The source galaxy is modeled using an `Inversion`.
+"""
+# %matplotlib inline
+# from pyprojroot import here
+# workspace_path = str(here())
+# %cd $workspace_path
+# print(f"Working Directory has been set to `{workspace_path}`")
+
+from pyprojroot import here
+import os
+import sys
+from os import path
+
+import autofit as af
+import autolens as al
+import autolens.plot as aplt
+"""
+__Dataset + Masking__
+
+Load, plot and mask the `Imaging` data.
+"""
+workspace_path = str(here())
+dataset_path = path.join(workspace_path, "Akanksha","slacs_new","slacs0252+0039")
+
+imaging = al.Imaging.from_fits(
+    image_path=path.join(dataset_path, "F814W_image.fits"),
+    noise_map_path=path.join(dataset_path, "F814W_noise_map.fits"),
+    psf_path=path.join(dataset_path, "F814W_psf.fits"),
+    pixel_scales=0.05,
+)
+
+mask = al.Mask2D.circular_annular(
+    shape_native=imaging.shape_native,
+    pixel_scales=imaging.pixel_scales,
+    inner_radius=0.25,
+    outer_radius=1.6,
+)
+
+imaging = imaging.apply_mask(mask=mask)
+
+imaging_plotter = aplt.ImagingPlotter(imaging=imaging)
+imaging_plotter.subplot_imaging()
+
+"""
+__Paths__
+
+The path the results of all chained searches are output:
+"""
+path_prefix = path.join(workspace_path, "Akanksha","SLACS1_OtherPipeline_output")
+
+"""
+__Redshifts__
+
+The redshifts of the lens and source galaxies, which are used to perform unit converions of the model and data (e.g.
+from arc-seconds to kiloparsecs, masses to solar masses, etc.).
+
+In this analysis, they are used to explicitly set the `mass_at_200` of the elliptical NFW dark matter profile, which is
+a model parameter that is fitted for.
+"""
+redshift_lens = 0.2803
+redshift_source =  0.9818
+
+"""
+__Model + Search + Analysis + Model-Fit (Search 1)__
+
+In search 1 we fit a lens model where:
+
+ - The lens galaxy's light is a parametric `EllSersic` bulge and `EllExponential` disk, the centres of
+ which are aligned [11 parameters].
+
+ - The lens galaxy's mass and source galaxy are omitted.
+
+The number of free parameters and therefore the dimensionality of non-linear parameter space is N=11.
+"""
+bulge = af.Model(al.lp.EllSersic)
+disk = af.Model(al.lp.EllExponential)
+
+bulge.centre = disk.centre
+
+bulge.centre.centre_0 = af.GaussianPrior(
+    mean=0.0, sigma=0.1, lower_limit=-5.0, upper_limit=5.0
+)
+bulge.centre.centre_1 = af.GaussianPrior(
+    mean=0.0, sigma=0.1, lower_limit=-5.0, upper_limit=5.0
+)
+
+disk.centre.centre_0 = af.GaussianPrior(
+    mean=0.0, sigma=0.1, lower_limit=-5.0, upper_limit=5.0
+)
+disk.centre.centre_1 = af.GaussianPrior(
+    mean=0.0, sigma=0.1,lower_limit=-5.0, upper_limit=5.0
+)
+
+model = af.Collection(
+    galaxies=af.Collection(
+        lens=af.Model(al.Galaxy, redshift=redshift_lens, bulge=bulge, disk=disk)
+    )
+)
+
+positions = al.Grid2DIrregular.from_json(
+    file_path=path.join(workspace_path, "Akanksha", "slacs_new","slacs0252+0039", "positions.json")
+)
+
+visuals_2d = aplt.Visuals2D(positions=positions)
+
+imaging_plotter = aplt.ImagingPlotter(imaging=imaging, visuals_2d=visuals_2d)
+imaging_plotter.subplot_imaging()
+
+settings_lens = al.SettingsLens(positions_threshold=1.0)
+
+dataset_name = 'SLACS1_NewPipeline'
+
+search = af.DynestyStatic(
+    path_prefix=path_prefix,
+    name="search[1]_light[parametric]",
+    unique_tag=dataset_name,
+    nlive=50,
+)
+
+analysis = al.AnalysisImaging(
+    dataset=imaging, positions=positions, settings_lens=settings_lens
+)
+
+result_1 = search.fit(model=model, analysis=analysis)
+
+"""
+__Model + Search + Analysis + Model-Fit (Search 2)__
+
+We use the results of search 1 to create the lens model fitted in search 2, where:
+
+ - The lens galaxy's light and stellar mass is an `EllSersic` bulge and `EllExponential` disk [Parameters
+ fixed to results of search 1].
+
+ - The lens galaxy's dark matter mass distribution is a `SphNFW` whose centre is aligned with the
+ `EllSersic` bulge and stellar mass model above [3 parameters].
+
+ - The lens mass model also includes an `ExternalShear` [2 parameters].
+
+ - The source galaxy's light is a parametric `EllSersic` [7 parameters].
+
+The number of free parameters and therefore the dimensionality of non-linear parameter space is N=12.
+
+NOTES:
+
+ - By using the fixed `bulge` and `disk` model from the result of search 1, we are assuming this is a sufficiently
+ accurate fit to the lens's light that it can reliably represent the stellar mass.
+"""
+bulge = result_1.instance.galaxies.lens.bulge
+disk = result_1.instance.galaxies.lens.disk
+
+dark = af.Model(al.mp.SphNFW)
+dark.centre = bulge.centre
+#dark.mass_at_200 = af.LogUniformPrior(lower_limit=1e8, upper_limit=1e15)
+dark.redshift_object = redshift_lens
+dark.redshift_source = redshift_source
+
+source_bulge=af.Model(al.lp.EllSersic)
+source_bulge.centre.centre_0 = af.GaussianPrior(
+    mean=0.0, sigma=0.1, lower_limit=-5.0, upper_limit=5.0
+)
+source_bulge.centre.centre_1 = af.GaussianPrior(
+    mean=0.0, sigma=0.1, lower_limit=-5.0, upper_limit=5.0
+)
+
+model = af.Collection(
+    galaxies=af.Collection(
+        lens=af.Model(
+            al.Galaxy,
+            redshift=redshift_lens,
+            bulge=bulge,
+            disk=disk,
+            dark=dark,#af.Model(al.mp.EllNFW),
+            shear=al.mp.ExternalShear,
+        ),
+        source=af.Model(al.Galaxy, redshift=redshift_source, bulge=source_bulge),
+    )
+)
+
+search = af.DynestyStatic(
+    path_prefix=path_prefix,
+    name="search[2]_light[fixed]_mass[light_dark]_source[parametric]",
+    unique_tag=dataset_name,
+    nlive=75,
+)
+
+analysis = al.AnalysisImaging(dataset=imaging)
+
+result_2 = search.fit(model=model, analysis=analysis)
+
+"""
+__Model + Search + Analysis + Model-Fit (Search 3)__
+
+We use the results of searches 1 and 2 to create the lens model fitted in search 3, where:
+
+ - The lens galaxy's light and stellar mass is a parametric `EllSersic` bulge and `EllExponential` disk
+ [8 parameters: priors initialized from search 1].
+
+ - The lens galaxy's dark matter mass distribution is a `EllNFWMCRLudlow` whose centre is aligned with the
+ `EllSersic` bulge and stellar mass model above [3 parameters: priors initialized from search 2].
+
+ - The lens mass model also includes an `ExternalShear` [2 parameters: priors initialized from search 2].
+
+ - The source galaxy's light is a parametric `EllSersic` [7 parameters: priors initialized from search 2].
+
+The number of free parameters and therefore the dimensionality of non-linear parameter space is N=22.
+
+Notes:
+
+ - This search attempts to address any issues there may have been with the bulge's stellar mass model.
+"""
+bulge = result_1.model.galaxies.lens.bulge
+disk = result_1.model.galaxies.lens.disk
+
+dark = result_2.model.galaxies.lens.dark
+dark.centre = bulge.centre
+
+model = af.Collection(
+    galaxies=af.Collection(
+        lens=af.Model(
+            al.Galaxy,
+            redshift=redshift_lens,
+            bulge=bulge,
+            disk=disk,
+            dark=dark,
+            shear=result_2.model.galaxies.lens.shear,
+        ),
+        source=af.Model(
+            al.Galaxy, redshift=redshift_source, bulge=result_2.model.galaxies.source.bulge
+        ),
+    )
+)
+
+search = af.DynestyStatic(
+    path_prefix=path_prefix,
+    name="search[3]_light[parametric]_mass[light_dark]_source[parametric]",
+    unique_tag=dataset_name,
+    nlive=100,
+)
+
+analysis = al.AnalysisImaging(dataset=imaging)
+
+result_3 = search.fit(model=model, analysis=analysis)
+
+"""
+__Model + Search + Analysis + Model-Fit (Search 4)__
+
+We use the results of searches 3 to create the lens model fitted in search 4, where:
+
+ - The lens galaxy's light and stellar mass is an `EllSersic` bulge and `EllExponential`
+ disk [Parameters fixed to results of search 3].
+
+ - The lens galaxy's dark matter mass distribution is a `EllNFWMCRLudlow` [Parameters fixed to results of
+ search 3].
+
+ - The lens mass model also includes an `ExternalShear` [Parameters fixed to results of search 3].
+
+ - The source-galaxy's light uses a `VoronoiMagnification` pixelization [2 parameters].
+
+ - This pixelization is regularized using a `Constant` scheme [1 parameter].
+
+The number of free parameters and therefore the dimensionality of non-linear parameter space is N=3.
+
+NOTES:
+
+ - This search allows us to very efficiently set up the resolution of the pixelization and regularization coefficient
+ of the regularization scheme, before using these models to refit the lens mass model.
+"""
+model = af.Collection(
+    galaxies=af.Collection(
+        lens=af.Model(
+            al.Galaxy,
+            redshift=redshift_lens,
+            bulge=result_3.instance.galaxies.lens.bulge,
+            disk=result_3.instance.galaxies.lens.disk,
+            dark=result_3.instance.galaxies.lens.dark,
+            shear=result_3.instance.galaxies.lens.shear,
+        ),
+        source=af.Model(
+            al.Galaxy,
+            redshift=redshift_source,
+            pixelization=al.pix.VoronoiMagnification,
+            regularization=al.reg.Constant,
+        ),
+    )
+)
+
+search = af.DynestyStatic(
+    path_prefix=path_prefix,
+    name="search[4]_light[fixed]_mass[fixed]_source[inversion_initialization]",
+    unique_tag=dataset_name,
+    nlive=20,
+)
+
+analysis = al.AnalysisImaging(dataset=imaging)
+
+result_4 = search.fit(model=model, analysis=analysis)
+
+"""
+__Model +  Search (Search 5)__
+
+We use the results of searches 3 and 4 to create the lens model fitted in search 5, where:
+
+ - The lens galaxy's light and stellar mass is an `EllSersic` bulge and `EllExponential`
+ disk [11 parameters: priors initialized from search 3].
+
+ - The lens galaxy's dark matter mass distribution is a `EllNFWMCRLudlow` [8 parameters: priors initialized
+ from search 3].
+
+The lens mass model also includes an `ExternalShear` [2 parameters: priors initialized from search 3].
+
+ - The source-galaxy's light uses a `VoronoiMagnification` pixelization [parameters fixed to results of search 4].
+
+ - This pixelization is regularized using a `Constant` scheme [parameters fixed to results of search 4].
+
+The number of free parameters and therefore the dimensionality of non-linear parameter space is N=19.
+"""
+model = af.Collection(
+    galaxies=af.Collection(
+        lens=af.Model(
+            al.Galaxy,
+            redshift=redshift_lens,
+            bulge=result_3.model.galaxies.lens.bulge,
+            disk=result_3.model.galaxies.lens.disk,
+            dark=result_3.model.galaxies.lens.dark,
+            shear=result_3.model.galaxies.lens.shear,
+        ),
+        source=af.Model(
+            al.Galaxy,
+            redshift=redshift_source,
+            pixelization=result_4.instance.galaxies.source.pixelization,
+            regularization=result_4.instance.galaxies.source.regularization,
+        ),
+    )
+)
+
+search = af.DynestyStatic(
+    path_prefix=path_prefix,
+    name="search[5]_light[parametric]_mass[light_dark]_source[inversion]",
+    unique_tag=dataset_name,
+    nlive=20,
+)
+
+"""
+__Positions + Analysis + Model-Fit (Search 5)__
+
+We update the positions and positions threshold using the previous model-fitting result (as described
+ in `chaining/examples/parametric_to_inversion.py`) to remove unphysical solutions from the `Inversion` model-fitting.
+"""
+settings_lens = al.SettingsLens(
+    positions_threshold=result_4.positions_threshold_from(
+        factor=3.0, minimum_threshold=0.2
+    )
+)
+
+analysis = al.AnalysisImaging(
+    dataset=imaging,
+    settings_lens=settings_lens,
+    positions=result_4.image_plane_multiple_image_positions,
+)
+
+result_5 = search.fit(model=model, analysis=analysis)
+
+"""
+Finish.
+"""
